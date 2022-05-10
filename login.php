80e65bd55f684acbff3b7b27b9756c521648195d
johndoeonlineftc <johndoeonlineftc@gmail.com>
2022-05-10 08:17:10 -0400

    added some comments

--- login.php
+++ login.php
@@ -1,11 +1,11 @@
 <?php      
     $host = "localhost";  
     $user = "mysqluser";  
-    $password = "mysupersecretpassword"; 
+    $password = ""; 
     $db_name = "events"; 
-    
-    //Flag: 9c7406a76dd8958ea0026e4756dca4346d1452b8 
-          
+              
+
+    // connect with db and credentials
     $con = mysqli_connect($host, $user, $password, $db_name);  
     if(mysqli_connect_errno()) {  
         die();  
