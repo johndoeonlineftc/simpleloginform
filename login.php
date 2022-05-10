a49deff21efc67f2dc4794580b26cb2003889a85
johndoeonlineftc <johndoeonlineftc@gmail.com>
2022-05-10 08:19:23 -0400

    fixed bug with error message

--- login.php
+++ login.php
@@ -7,6 +7,6 @@
     // connect with db and credentials
     $con = mysqli_connect($host, $user, $password, $db_name);
     if(mysqli_connect_errno()) {
-        die( die("Failed to connect with MySQL: ". mysqli_connect_error());  );
+ 	die("Failed to connect with MySQL: ". mysqli_connect_error());
     }
 ?>
